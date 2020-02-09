0306d508764fc8ff4954f5588225ede205f9ef1c
Virendra Arekar <virendra.arekar@gmail.com>
2020-02-10 03:59:45 +0530

    first upload

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with justify-content-center">Made with <i class="fa fa-heart heart"></i> by VIRENDRA AREKAR</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
